53967f6ba338e9d156e2d2db8abac8ebf2cb3f78
heli-xu <91294567+heli-xu@users.noreply.github.com>
2024-01-17 21:17:06 -0500

    update qmd with maps, better descrp.

--- data_load.R
+++ data_load.R
@@ -14,3 +14,20 @@ write.csv(skim_calles,"data/skim_calles.csv")
 zat_data <- read_xlsx("data/ZAT_INDICADORES.xlsx")
 
 skim_zat <- skim(zat_data)
+
+
+
+## Ran
+
+sfa = calle |> 
+  slice(1:1000) |>
+  st_transform(crs = st_crs("+proj=longlat +datum=WGS84"))
+
+saveRDS(calles_1000, "data/calles_1000.rds")
+
+glimpse(sfa)
+
+sfa |>
+  leaflet() |>
+  addTiles() |>
+  leaflet::addPolylines()
