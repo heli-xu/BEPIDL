5d20cfb6189e2bd8669e1b0622dc04a1b478ca59
Heli Xu <xuheli91@gmail.com>
2024-01-30 14:47:36 -0500

    add derived zat var, and shiny datasets

--- clean_data/zat.R
+++ clean_data/zat.R
@@ -21,5 +21,24 @@ georef_zat <- zat_data %>%
 
 saveRDS(georef_zat, file = "clean_data/ZAT/georef_zat.rds")
 
+georef_zat <- readRDS("../../clean_data/ZAT/georef_zat.rds")
 
+georef_zat_xtr <- georef_zat %>%
+  mutate(STTREESPROAD = NUMSTTREES/LONGMV,
+         NUMBRIDGESAREA = NUMBRIDGES/areakm2,
+         PEDLIGHTTRAFLIGHT = NUMPTFLIGH/NUMTTFLIGH,
+         TRAFLIGHTINTS = NUMPTFLIGH/NUMINT,
+         .before = geometry) 
 
+# for shinylive -----------------------------------------------------------
+
+zat_indicator_list <- georef_zat_xtr %>% 
+  as.data.frame() %>% 
+  select(-(1:3), -geometry, -c(MUNCod, NOMMun, UTAM, Area)) %>% 
+  colnames()
+
+save(zat_indicator_list, file = "R/zat_indicator_list.rda")
+
+save(georef_zat_xtr, file = "posts/shinylive-zat/georef_zat_xtra.rda")
+  
+zat_indicator_list
