98abebdcf126086cb4c4ba510553adfe1f6455eb
Heli Xu <xuheli91@gmail.com>
2024-02-17 13:42:33 -0500

    add map to shiny app, improve look

--- analysis/shiny-zat/app.R
+++ analysis/shiny-zat/app.R
@@ -9,11 +9,14 @@
 
 library(shiny)
 library(dplyr)
+library(sf)
 library(ggplot2)
+library(leaflet)
+library(glue)
 
 
-load("R/zat_indicator_list.rda")
-
+load("R/zat_indicator_table.rda")
+  
 ui <- fluidPage(
 
 tags$head(includeCSS("CSS/Header.css")),
@@ -23,17 +26,19 @@ tags$head(includeCSS("CSS/styles.css")),
   includeHTML("HTML/Header.html"),
   
       # Application title
-      titlePanel("Distribution of ZAT-level Indicators"),
+  div( titlePanel("Built Environment Features in Bogotá: ZAT-level Indicators"), id = "navbar"),
 
-      # Sidebar with a slider input for number of bins
+      # Sidebar 
       sidebarLayout(
           sidebarPanel(
-              selectInput("indicator",
-                          "ZAT-level indicator:",
-                          choices = zat_indicator_list,
-                          selected = "BUSTOPDENS"),
+            width = 3,
+            selectInput("indicator",
+                        "ZAT-level indicator:",
+                        choices = zat_indicator_table$var,
+                        selected = "BUSTOPDENS"),
               
-              textOutput("description")
+            textOutput("description_title"),
+            textOutput("description")
           ),
           
 
@@ -41,46 +46,116 @@ tags$head(includeCSS("CSS/styles.css")),
           mainPanel(
             width = 9,
             column(6, plotOutput("distPlot")),
-            column(6, plotOutput("map"))
+            column(6, leafletOutput("map"))
           )
-      )
+      ),
+  
+  ## Footer
+  includeHTML("HTML/Footer.html")
 )
 
 server <- function(input, output, session) {
-  load("R/zat_std.rda")
+  load("R/zat_std_geo.rda")
+  
+  output$description_title <- renderText({print("Indicator Description:")})
+  
+  output$description <- renderText({
+    zat_indicator_table %>% 
+      filter(var == input$indicator) %>% 
+      pull(description)
+  })
   
   output$distPlot <- renderPlot({
-    df_to_plot<- zat_std %>% 
-                 select(input$indicator) 
+    print(input$indicator)
     
-    mean_value <- mean(df_to_plot[[1]])
-    sd_value <- sd(df_to_plot[[1]])
     
-    ggplot(df_to_plot, aes(.data[[input$indicator]])) +
-      geom_histogram(fill = "skyblue", color = "blue") +
-      geom_vline(xintercept = mean_value, color = "#002f6c", #drexel blue
-        size = 2) +
-      geom_vline( xintercept = mean_value + sd_value, color = "#f2ca00",
-        linetype = "dotted",size = 2
-      ) +
-      geom_vline(xintercept = mean_value - sd_value, color = "#f2ca00",
-        linetype = "dotted",size = 2
-       ) +
-      annotate("text", x = mean_value, y = -5, label = "Mean",  
-               size = 8, color = "#808080") +
-      annotate("text", x = mean_value + sd_value, y =-5,label = "+1 SD",
-                size = 8,  color = "#808080") +
-      annotate("text", x = mean_value - sd_value, y = -5, label = "-1 SD", 
-                size = 8, color = "#808080")+
-      theme_minimal() +
-      ylab("Number of ZAT Units")+
-      theme(
-        panel.grid.minor = element_blank(),
-        axis.text.x = element_text(size = 14),
-        axis.text.y = element_text(size = 14, face = "bold"),
-        axis.title.x = element_text(size = 15, face = "bold"),
-        axis.title.y = element_text(size = 15, face = "bold")
-        )
+    # df_to_plot_old<- zat_std %>% 
+    #   as.data.frame() |>
+    #   select(input$indicator) 
+    # df_to_plot = tibble(column_selected = vector_selected)
+    
+    vector_selected <- zat_std_geo |> pull(input$indicator)
+    
+    mean_value <- mean(vector_selected)
+    sd_value <- sd(vector_selected)
+    
+    
+    
+    zat_std_geo|>
+      ggplot(aes(.data[[input$indicator]])) +
+        geom_histogram(fill = "skyblue", color = "blue") +
+        geom_vline(xintercept = mean_value, color = "#002f6c", #drexel blue
+          size = 2) +
+        geom_vline( xintercept = mean_value + sd_value, color = "#f2ca00",
+          linetype = "dotted",size = 2) +
+        geom_vline(xintercept = mean_value - sd_value, color = "#f2ca00",
+          linetype = "dotted",size = 2) +
+        annotate("text", x = mean_value, y = -5, label = "Mean",
+               size = 4, color = "#808080") +
+        annotate("text", x = mean_value + sd_value, y =-5,label = "+1 SD",
+                size = 4,  color = "#808080") +
+        annotate("text", x = mean_value - sd_value, y = -5, label = "-1 SD",
+                size = 4, color = "#808080")+
+        theme_minimal() +
+        labs(
+          title = "Histogram of the Distribution",
+            y = "Number of ZAT Units")+
+        theme(
+          panel.grid.minor = element_blank(),
+          title = element_text(size = 15, face = "bold"),
+          axis.text.x = element_text(size = 14),
+          axis.text.y = element_text(size = 14, face = "bold"),
+          axis.title.x = element_text(size = 15, face = "bold"),
+          axis.title.y = element_text(size = 15, face = "bold")
+          )
+  })
+
+  #leaflet map color palette
+  
+  output$map <- renderLeaflet({
+    zat_df <- as.data.frame(zat_std_geo)
+
+    domain_v <- zat_df %>%
+      select(input$indicator) %>%
+      pull()
+
+    pal <- colorNumeric(
+      palette = c("orange","navy"),
+      domain = domain_v
+    )
+
+    zat_label <- glue("ZAT{zat_std_geo$ZAT} {input$indicator}: {round(domain_v,2)}/km2")
+
+    map_title <- glue("{input$indicator} </br>in Bogotá")
+
+    zat_std_geo %>%
+      st_zm() %>%
+      st_transform(crs = st_crs("+proj=longlat +datum=WGS84")) %>%
+      leaflet() %>%
+      addProviderTiles(providers$CartoDB.Voyager)  %>%
+      addPolygons(color = "white",
+        weight = 0.5,
+        smoothFactor = 0.5,
+        opacity = 1,
+        fillColor = ~pal(domain_v),
+        fillOpacity = 0.8,
+        highlightOptions = highlightOptions(
+          weight = 5,
+          color = "#666",
+          fillOpacity = 0.8,
+          bringToFront = TRUE),
+        label = zat_label,
+        labelOptions = labelOptions(
+          style = list(
+            "font-family" = "Fira Sans, sans-serif",
+            "font-size" = "1.2em"
+          ))
+      ) %>%
+      addLegend("bottomright",
+        pal = pal,
+        values = ~domain_v,
+        title = map_title,
+        opacity = 1)
   })
 }
 
