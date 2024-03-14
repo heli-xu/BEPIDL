0451f815d16aafcb15c3fea0e49b1c5e2bb69b05
Heli Xu <xuheli91@gmail.com>
2024-03-13 22:28:09 -0400

    update hclust_geo vis + datasets (ignored)

--- clean_data/zat.R
+++ clean_data/zat.R
@@ -527,17 +527,34 @@ cluster_plot <- function(cluster_data) {
      ggplot() +
      geom_col(aes(x = factor(indicator), y = scaled), fill = pal[cluster_data$clus]) +
      coord_flip() + 
-    geom_hline(yintercept = 0, linetype = "dotted")+
+     geom_hline(yintercept = 0, linetype = "dotted")+ #still set as y (although it's after flipping)
      theme_minimal() +
-     labs(y = "Relatively less    more           Relatively less  more",
+     labs(y = "Relative Less          Relative More",
           x = "") +
+    scale_x_discrete(expand = expansion(mult = 0.005),
+                     labels = c(
+                       "mean_BUSTOPDENS"="Bus stop density" ,
+                       "mean_road_length_log" ="Road length",
+                       "mean_st_4ln_length_log" ="Street length",
+                       "mean_bikelane_m_log" ="Bikelane length",
+                       "mean_sttree_per_km2" ="Tree",
+                       "mean_bridg_per_km2" ="Bridge",
+                        "mean_trlight_per_km2" ="Traffic light" ,
+                        "mean_numrbp_per_km2" ="Bus route count" ,
+                        "mean_numrt_per_km2" ="BRT route count",
+                       "mean_bus_length_log" ="Bus route length",
+                       "mean_brt_length_log"="Bus route length" 
+                     ))+
      theme(panel.grid = element_blank(),
            axis.text.x = element_blank(),
-           axis.text.y = element_text(),
+           axis.text.y = element_text(size = 9),
            axis.line.x = element_line(arrow = grid::arrow(length = unit(0.3, "cm"), 
                                                           ends = "both")),
-           plot.margin=grid::unit(c(0,0,0,0), "mm")) +
-    facet_wrap(~clus)
+           plot.margin=grid::unit(c(0,0,0,0), "mm"),
+           strip.text = element_text(face = "bold", size = 10),
+           strip.background = element_rect(fill = "grey", color = "white"),
+           panel.spacing.x = unit(1, "lines")) +
+    facet_wrap(~clus, ncol = 1)
 }
 
 cluster_plot(zat_hclust)
@@ -549,4 +566,39 @@ hclust_geo <- zat %>%
 map <- ggplot()+
   geom_sf(data = hclust_geo, fill = pal[hclust_geo$clus])
 
-cluster_plot(zat_hclust) | map
+(cluster_plot(zat_hclust) | map ) + 
+  plot_annotation('Hierarchical Clustering with Indicators only', subtitle = 'ZAT level, Bogotá',
+                  theme=theme(plot.title=element_text(size=14, face = "bold", hjust=0.5),
+                              plot.subtitle = element_text(size = 10, face = "bold", hjust = 0.5)))
+
+clustgeo_geo <- zat %>% 
+  mutate(clus = p4_geo) %>% 
+  st_zm()
+
+map2 <- ggplot()+
+  geom_sf(data = clustgeo_geo, fill = pal[clustgeo_geo$clus])
+
+clustnb_geo <- zat %>% 
+  mutate(clus = p4_nb) %>% 
+  st_zm()
+
+map3 <- ggplot()+
+  geom_sf(data = clustnb_geo, fill = pal[clustnb_geo$clus])
+
+(cluster_plot(zat_clustgeo) | map2 ) + 
+  plot_annotation('Hierarchical Clustering with Indicators and Spatial Constraint', subtitle = 'ZAT level, Bogotá',
+                  theme=theme(plot.title=element_text(size=14, face = "bold", hjust=0.5),
+                              plot.subtitle = element_text(size = 10, face = "bold", hjust = 0.5)))
+
+(cluster_plot(zat_clustnb) | map3 ) + 
+  plot_annotation('Hierarchical Clustering with Indicators and Neighborhood Constraint', subtitle = 'ZAT level, Bogotá',
+                  theme=theme(plot.title=element_text(size=14, face = "bold", hjust=0.5),
+                              plot.subtitle = element_text(size = 10, face = "bold", hjust = 0.5)))
+
+saveRDS(zat, file = "ZAT/zat_shapefile.rds")
+saveRDS(zat_hclust, file = "hclust_geo/zat_hclust.rds")
+saveRDS(zat_clustgeo, file = "hclust_geo/zat_clustgeo.rds")
+saveRDS(zat_clustnb, file = "hclust_geo/zat_clustnb.rds")
+saveRDS(hclust_geo, file = "hclust_geo/hclust_geo.rds")
+saveRDS(clustgeo_geo, file = "hclust_geo/clustgeo_geo.rds")
+saveRDS(clustnb_geo, file = "hclust_geo/clustnb_geo.rds")
