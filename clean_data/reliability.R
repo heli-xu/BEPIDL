f4572cb08fbd5ee00cc4615c5fff1784db9677f4
Heli Xu <xuheli91@gmail.com>
2024-04-03 16:31:50 -0400

    predict-gis clean, cor matrix, ROC curves

--- clean_data/reliability.R
+++ clean_data/reliability.R
@@ -4,7 +4,7 @@ library(jsonlite)
 library(foreign)
 library(sf)
 library(leaflet)
-
+library(janitor)
 #import data --------------------------
 training <- read_csv("../data/MLdata_GIS/annotations.csv")
 
@@ -81,12 +81,17 @@ predict_gis %>%
     weight = 1, fillColor = 'blue', color = 'blue') 
 
 predict_gis_clean <- predict_gis %>% 
-  select(-c(Join_Count, TARGET_FID, Join_Cou_1,
-    TARGET_F_1, OBJECTID_1, Join_Cou_2, CODIGO_IDE,
-    TARGET_F_2, FID_EP_IND, OID_, Etiquetas, Etiqueta_1,
-    CODIGO_CRI, CODIGO_ZON, OBJECTID_2, CODIGO_MAN
+  rename_with(~ tolower(.)) %>%  #clean_names()add "_"
+  select(-c(
+    target_f_1, latitude, longitude, objectid_1, join_cou_2, 
+    target_f_2, fid_ep_ind, objectid_2, codigo_zon, codigo_cri, 
+    normativa, acto_admin, numero_act, fecha_acto, escala_cap, 
+    fecha_capt, responsabl, mviccalzad, mviccat, mvinombre, 
+    mvinaltern, mvinprinci, mvingenera, mvinantigu, mvietiquet, 
+    mvisvia, mviciv, mvicodigo, mvinumc, mvivelreg, point_coun
     )) %>% 
-  rename_with(~ tolower(.), area:ESTRATO) %>% 
+  rename(gsv_yr = date) %>% 
+  rename_with(~ paste0("an_", .), sign_traff:potholes) %>% 
   rename(
       area_calle = area,
       gis_trees = arboles,
@@ -125,11 +130,12 @@ predict_gis_clean <- predict_gis %>%
       pedbike_col = sum_atrope,
       ses_level = estrato
   ) %>% 
-  rename_with(~ paste0("pr_", .), Date:Potholes) %>% 
-  drop_na(CodigoCL) %>%
+  drop_na(codigocl) 
+
+predict_gis_clean2 <- predict_gis_clean %>%
   mutate(
-    MVITCla = case_match(
-      MVITCla,
+    mvitcla = case_match(
+      mvitcla,
       0 ~ "missing",
       1 ~ "Arterial",
       2 ~ "Collector",
@@ -140,42 +146,156 @@ predict_gis_clean <- predict_gis %>%
       7 ~ "Projected"
     ),
     road_type = case_match(
-      MVITCla,
+      mvitcla,
       c("missing","Pedestrian", "Rural","Unknown","Projected") ~ "Other",
-      .default = MVITCla
+      .default = mvitcla
     )
   ) %>% 
   mutate(
-    gis_median = case_when(
-      area_median > 0 ~ 1,
-      .default = 0
+    gis_median = if_else(area_median > 0, 1, 0),
+    gis_sw = if_else(area_sidewalk > 0, 1, 0),
+    gis_bike_lane = if_else(bike_length > 0, 1, 0),
+    gis_any_bus = if_else(gis_bus_lanes > 0, 1, 0),
+    gis_brt_yn = if_else(brt_routes > 0, 1, 0),
+    st_dir = case_when(
+      sent_vial == "doble" ~ 0,
+      sent_vial == "uno" ~ 1
     ),
-    gis_sidewalk = case_when(
-      area_sidewalk >0 ~ 1,
-      .default = 0
+    gis_veh_br = if_else(gis_vehicle_bridge == "vehicular", 1, 0, missing = 0),
+    gis_ped_br = if_else(gis_ped_bridge == "Peatonal", 1, 0, missing = 0)
+    ) %>% 
+  mutate(
+    across(c(an_sign_traff:an_potholes, gis_trees, gis_bus_stops, 
+             gis_road_signs, gis_traffic_lights, gis_road_signs_inv, 
+             gis_stop_signs, gis_traffic_fines_tot, gis_lturn_sign, 
+             gis_bike_signs, gis_bus_signs, gis_pedxwalk_signs, 
+             gis_speed_bump_signs, gis_stop_signs2, gis_parking_signs, 
+             gis_school_zone_signs, gis_yield_signs, gis_total_st_signs),
+           ~if_else(.>0, 1, 0, missing = 0), .names = "{.col}_yn")
+  ) %>% 
+  mutate(
+    any_ped = if_else(si_act_pea >0, 1, 0),
+    an_pedxwalk_yn = if_else(an_sign_cross_yn == 1 | an_pedestrian_yn == 1 | an_crosswalk_yn == 1, 
+                          true = 1, 
+                          false = 0),
+    ses_rev = case_match(
+      ses_level,
+      6~0, 5~1, 4~2, 2~4, 1~5, 0~6
     ),
-    gis_bike_lane = case_when(
-      bike_length > 0 ~ 1,
-      .default = 0
-    ),
-    gis_vehicle_bridge = case_match(
-      gis_vehicle_bridge,
-      NA ~ 0,
-      .default = 1
-    ),
-    gis_ped_bridge = case_match(
-      gis_ped_bridge,
-      NA ~ 0, 
-      .default = 1
+    road_seg2 = case_match(
+      road_segments,
+      6 ~ 5,
+      .default = road_segments
     )
-    
+  ) %>% 
+  mutate(
+    across(ends_with("_yn"), ~factor(.))
   )
+  # mutate(
+  #   gis_median = case_when(
+  #     area_median > 0 ~ 1,
+  #     .default = 0
+  #   ),
+  #   gis_sidewalk = case_when(
+  #     area_sidewalk >0 ~ 1,
+  #     .default = 0
+  #   ),
+  #   gis_bike_lane = case_when(
+  #     bike_length > 0 ~ 1,
+  #     .default = 0
+  #   ),
+  #   gis_vehicle_bridge = case_match(
+  #     gis_vehicle_bridge,
+  #     NA ~ 0,
+  #     .default = 1
+  #   ),
+  #   gis_ped_bridge = case_match(
+  #     gis_ped_bridge,
+  #     NA ~ 0, 
+  #     .default = 1
+  #   )
+  #   
+  # )
 
-# st_dir still not sure
+#c(an_sign_traff-an_potholes, gis_trees, gis_bus_stops, gis_road_signs, gis_traffic_lights, gis_road_signs_inv, gis_stop_signs, gis_traffic_fines_tot, gis_lturn_sign, gis_bike_signs, gis_bus_signs, gis_pedxwalk_signs, gis_speed_bump_signs, gis_stop_signs2, gis_parking_signs, gis_school_zone_signs, gis_yield_signs, gis_total_st_signs, area_calle, grade, area_roadway, p_ancho_cl, area_median, area_sidewalk, brt_routes, bus_routes, speed_limit, num_lanes_total, num_lanes_avg, administra:total_gene)
 
-na2 <- predict_gis %>% 
-  filter(is.na(CodigoCL))
-#got data..check if can join to a street
+# correlation -------------------------
+predict_gis_cor <- predict_gis_clean2 %>% 
+  select(c(an_sign_traff:an_potholes, gis_trees, gis_bus_stops, gis_road_signs, gis_traffic_lights, gis_road_signs_inv, gis_stop_signs, gis_traffic_fines_tot, gis_lturn_sign, gis_bike_signs, gis_bus_signs, gis_pedxwalk_signs, gis_speed_bump_signs, gis_stop_signs2, gis_parking_signs, gis_school_zone_signs, gis_yield_signs, gis_total_st_signs, area_calle, grade, area_roadway, p_ancho_cl, area_median, area_sidewalk, brt_routes, bus_routes, speed_limit, num_lanes_total, num_lanes_avg, administra:total_gene)) %>% 
+  cor() %>% 
+  as.data.frame()
+
+write_csv(predict_gis_cor, file = "MLdata_GIS/predict_gis_cor.csv")
+
+
+# ROC -------------------
+library(pROC)
+
+# GIS-outcome/reference: yn
+# predictions-predictors: yn
+
+an_variables <- c(
+  "an_sign_traff_yn",
+  "an_sign_traff_yn",
+  "an_traffic_li_yn",
+  "an_pedxwalk_yn",
+  "an_sign_stop_yn",
+  "an_sign_stop_yn",
+  "an_sign_yield_yn",
+  "an_sign_schoo_yn",
+  "an_sidewalk_yn",
+  "an_lane_bike_yn",
+  "an_lane_bus_yn",
+  "an_median_yn",
+  "an_speed_bump_yn",
+  "an_trees_yn",
+  "an_bus_stop_yn",
+  "an_parked_veh_yn",
+  "an_lane_parki_yn",
+  "an_brt_statio_yn"
+)
+
+# Variables starting with "gis_"
+gis_variables <- c(
+  "gis_road_signs_inv_yn",
+  "gis_total_st_signs_yn",
+  "gis_traffic_lights_yn",
+  "gis_pedxwalk_signs_yn",
+  "gis_stop_signs_yn",
+  "gis_stop_signs2_yn",
+  "gis_yield_signs_yn",
+  "gis_school_zone_signs_yn",
+  "gis_sw",
+  "gis_bike_lane",
+  "gis_any_bus",
+  "gis_median",
+  "gis_speed_bump_signs",
+  "gis_trees_yn",
+  "gis_bus_stops_yn",
+  "gis_parking_signs_yn",
+  "gis_parking_signs_yn",
+  "gis_brt_yn"
+)
+
+roc(predict_gis_clean2$gis_brt_yn, as.numeric(predict_gis_clean2$an_brt_statio_yn))
+
+res = map2(an_variables, gis_variables, 
+     \(x, y) roc(predict_gis_clean2[[y]], as.numeric(predict_gis_clean2[[x]])))
+
+label <- str_sub(gis_variables, 5)
+n <- c(1:length(gis_variables))
+colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", 
+            "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", "#aec7e8", "#ffbb78", 
+            "#98df8a", "#ff9896", "#c5b0d5", "#c49c94", "#f7b6d2", "#c98379")
+plot(res[[1]])
+map2(n, colors, \(x, y) plot(res[[x]], col = y, xlim = c(-0.5, 1.2), add = T))
+legend("bottomright", legend = label, col = colors, lty = 1, cex =0.8)
+
+    
+
+# na2 <- predict_gis %>% 
+#   filter(is.na(CodigoCL))
+# #got data..check if can join to a street
 
 # na2_join <- na2 %>% 
 #   st_as_sf(coords = c("Longitude","Latitude"), crs = st_crs(4326)) %>% 
