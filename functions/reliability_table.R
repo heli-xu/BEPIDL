6415637beae35acf0cbdbce1b9457b08876a093d
Heli Xu <xuheli91@gmail.com>
2024-04-10 16:50:24 -0400

    update reliability_table() function roxygen

--- functions/reliability_table.R
+++ functions/reliability_table.R
@@ -1,3 +1,24 @@
+#' Calculate reliability metrics from a table
+#'
+#' This function calculates reliability metrics including sensitivity, specificity, positive predictive value (PPV),
+#' negative predictive value (NPV), Cohen's kappa, and area under the curve (AUC) from a given data table and a reference variable.
+#'
+#' @param var A character vector specifying the names of variables of interest in the data.
+#' @param var_ref A character vector specifying the names of reference variables in the data. Usually GIS data.
+#' @param data A data frame containing the variables of interest and reference variables, with joining GIS data with training/prediction/annotator data at street level.
+#'
+#' @return A data frame containing reliability metrics including sensitivity, specificity, PPV, NPV, kappa, and AUC.
+#' 
+#' @importFrome pROC auc roc
+#' @importFrom caret sensitivity specificity posPredValue negPredValue 
+#' @importFrom epiR epi.kappa
+#' @importFrom purrr map2_dbl map2_dfr pluck
+#' @importFrom magrittr %>%
+#' 
+#' @export
+#' 
+#' 
+
 reliability_table <- function(var, var_ref, data){
   auc <- map2_dbl(var,var_ref, 
                   \(x, y) auc(data[[y]], as.numeric(data[[x]])))
