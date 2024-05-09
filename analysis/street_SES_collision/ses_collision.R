bdd165d01e131481dbb986d848a9f7a0631f4815
Heli Xu <xuheli91@gmail.com>
2024-05-09 10:54:18 -0400

    summary of street level ses~collision

--- analysis/street_SES_collision/ses_collision.R
+++ analysis/street_SES_collision/ses_collision.R
@@ -1,12 +1,15 @@
 library(tidyverse)
 library(sf)
 library(MASS)
+library(broom)
 
+#import data ----------------------------
 calle_df <- readRDS("../../clean_data/calles/calle_df.rds")
 
 ses_calle100m <- readRDS("../../clean_data/SES/ses_calle100m.rds")
 ses_calle500m <- readRDS("../../clean_data/SES/ses_calle500m.rds")
 
+#SES~ped collision -------------------------------
 calle_ped100 <- calle_df %>% 
   dplyr::select(CodigoCL, ped_inc = si_act_pea) %>% 
   left_join(
@@ -32,7 +35,9 @@ calle_ped500 <- calle_df %>%
   )
 
 fit500 <- glm.nb(ped_inc ~ ses_cat, data = calle_ped500)
-summary(fit2)
+
+fit500_df <- tidy(fit500)
 
 
-
+write_csv(fit100_df, file = "fit100.csv")
+write_csv(fit500_df, file = "fit500.csv")
