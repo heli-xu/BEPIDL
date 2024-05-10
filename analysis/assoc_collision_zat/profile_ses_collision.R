4426d306de85069485b5b84bade514f7db489725
Heli Xu <xuheli91@gmail.com>
2024-05-10 11:58:15 -0400

    update nb profile distribution in zat profile-collision assoc

--- analysis/assoc_collision_zat/profile_ses_collision.R
+++ analysis/assoc_collision_zat/profile_ses_collision.R
@@ -9,6 +9,18 @@ profile <- readRDS("../../clean_data/aggr_hclust_geo/calle2zat_geo.rds") %>%
 
 profile %>% filter(is.na(clus))
 
+# Profile distribution --------------------------------
+distr_stat <- function(data, unit, group){
+  data %>% 
+    dplyr::select({{unit}},{{group}}) %>% 
+    count({{group}}) %>% 
+    ungroup() %>% 
+    mutate(total = nrow(data), #as long as no NA rows
+      percent = (n/total)*100)
+}
+
+profile_distr <- distr_stat(profile, ZAT, clus) %>% 
+  mutate(clus = factor(clus)) #match var type
 
 # collision ~ profile ----------------------------------
 profile_ped_zat <- col_ped_zat %>% 
@@ -16,22 +28,36 @@ profile_ped_zat <- col_ped_zat %>%
   mutate(clus = factor(clus)) %>% 
   drop_na(clus)
 
+## pedestrian collision with injury --------------
+
 fit_injury <- glm.nb(injury ~ clus, data = profile_ped_zat)
 summary(fit_injury)
 injury_df <- tidy(fit_injury) %>% 
   mutate(outcome = 'injury')
 
-
+## pedestrian collision with death --------------
 fit_death <- glm.nb(death ~ clus, data = profile_ped_zat)
 summary(fit_death)
 death_df <- tidy(fit_death) %>% 
   mutate(outcome='death')
 
-
+## total pedestrian collision --------------
 fit_total <- glm.nb(total ~ clus, data = profile_ped_zat)
 summary(fit_total)
+
 total_df <- tidy(fit_total) %>% 
   mutate(outcome='total') %>% 
-  bind_rows(injury_df, death_df)
+  bind_rows(injury_df, death_df) %>% 
+  mutate(clus = case_match(
+    term,
+    "(Intercept)" ~ "1",
+    "clus2" ~ "2",
+    "clus3" ~ "3",
+    "clus4" ~ "4"
+  )) %>% 
+  left_join(profile_distr, by = "clus") %>% 
+  dplyr::select(clus:percent, estimate:outcome) %>% 
+  rename(nb_profile = clus,
+    percent_zat = percent)
 
 write_csv(total_df, file = "collision-profile_zat.csv")
