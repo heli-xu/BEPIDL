b72bc1850fd57d6fc888618e01997e2382efe83d
Heli Xu <xuheli91@gmail.com>
2024-05-10 10:35:51 -0400

    add percent of each SES by street

--- analysis/street_SES_collision/ses_collision.R
+++ analysis/street_SES_collision/ses_collision.R
@@ -9,13 +9,13 @@ calle_df <- readRDS("../../clean_data/calles/calle_df.rds")
 ses_calle100m <- readRDS("../../clean_data/SES/ses_calle100m.rds")
 ses_calle500m <- readRDS("../../clean_data/SES/ses_calle500m.rds")
 
-# SES descriptive stat ----------------------------
+# SES distribution ----------------------------
 ses_100 <- ses_calle100m %>% 
   dplyr::select(CodigoCL, ses_cat) %>% 
   count(ses_cat) %>% 
   ungroup() %>% 
   pivot_wider(names_from = ses_cat, values_from = n) %>% 
-  mutate(total_hs = rowSums(.)) %>% 
+  mutate(total = rowSums(.)) %>% 
   pivot_longer(cols = -total_hs, names_to = "ses_cat", values_to = "count") %>% 
   mutate(percent = (count/total_hs)*100)
 
@@ -33,6 +33,7 @@ distr_stat <- function(data, unit, group){
 # check it works
 ses_100 <- distr_stat(ses_calle100m, CodigoCL, ses_cat)
 
+ses_500 <- distr_stat(ses_calle500m, CodigoCL, ses_cat)
   
 # SES~ped collision -------------------------------
 ## 100m buffer------------------------------------
@@ -63,11 +64,10 @@ fit100_df <- tidy(fit100) %>%
       "ses_cat_r1" ~ "1"
     )) %>% 
   left_join(ses_100, by = "group") %>% 
-  dplyr::select(ses_level = group,
-    percent_household = )
+  dplyr::select(group:percent, estimate:buffer_m) %>% 
+  rename(ses_level = group,
+    percent_street = percent)
   
-
-
 
 ## 500m buffer--------------------------------------------
 calle_ped500 <- calle_df %>% 
@@ -84,7 +84,20 @@ fit500 <- glm.nb(ped_inc ~ ses_cat_r, data = calle_ped500)
 summary(fit500)
 
 fit500_df <- tidy(fit500) %>% 
-  mutate(buffer_m = 500) 
+  mutate(buffer_m = 500,
+    group = case_match(
+      term,
+      "(Intercept)" ~ "6",
+      "ses_cat_r5" ~ "5",
+      "ses_cat_r4" ~ "4",
+      "ses_cat_r3" ~ "3",
+      "ses_cat_r2" ~ "2",
+      "ses_cat_r1" ~ "1"
+    )) %>% 
+  left_join(ses_500, by = "group") %>% 
+  dplyr::select(group:percent, estimate:buffer_m) %>% 
+  rename(ses_level = group,
+    percent_street = percent)
 
 total_df <- bind_rows(fit100_df, fit500_df)
 
