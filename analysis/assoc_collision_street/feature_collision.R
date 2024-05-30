6d0e92570254dfe572265184a685152a795988c2
Heli Xu <xuheli91@gmail.com>
2024-05-29 23:35:23 -0400

    move plot_facet_RR() to function file

--- analysis/assoc_collision_street/feature_collision.R
+++ analysis/assoc_collision_street/feature_collision.R
@@ -222,6 +222,8 @@ sig <- feature_xtra %>%
   filter(p.value < 0.05)
 
 ## 1.6 Visualize ------------
+
+#below has become a function plot_facet_RR()
 feature_RR %>% 
   filter(!term == "(Intercept)",
     !term == "st_dir2") %>%  #st_dir2 no difference
@@ -257,41 +259,6 @@ feature_RR %>%
     strip.placement = "outside",
     axis.line = element_line()
   )
-
-plot_facet_RR <- function(data){
-  data %>% 
-    ggplot(aes(x = estimate, y = category))+
-    geom_rect(aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf, fill = predictor), alpha = 1) +
-    scale_fill_manual(values = rep(c("#ffffff00", "#f0f0f090"), 20), guide = "none")+ 
-    geom_errorbar(aes(xmax = conf.high, xmin = conf.low), linewidth = 0.5)+
-    geom_point(aes(x = estimate), size = 2)+
-    geom_vline(aes(xintercept = 1), linetype = 2) +
-    facet_grid(rows = vars(predictor), scales = "free", switch = "y")+
-    #use "free" because category not the same in each var (y axis)
-    scale_x_continuous(breaks = sort(round(c(seq(min(feature_RR$conf.low), max(feature_RR$conf.high), length.out = 4), 1), 0)))+
-    theme_bw()+
-    # scale_fill_manual(values = c("Medium" = "#ffffff00", "High" = "#f0f0f090"), guide = "none") +
-    labs(
-      title = "Pedestrian Collision and Street Features",
-      x = "RR (95%CI)",
-      y = "Street Features",
-      caption = "'st_dir' compares 'double' relative to 'one' direction. \nFor other street features, tertiles are used in analysis.\n Comparisons are relative to the 'Low' category."
-    )+
-    theme(
-      plot.title = element_text(size = 13, face = "bold", hjust = 0),
-      text = element_text(size = 11),
-      axis.title = element_text(size = 12),
-      # plot.title.position = "plot",
-      panel.spacing.y = unit(0, "points"),
-      panel.border = element_blank(),
-      #axis.text.y = element_blank(),
-      axis.ticks.length.y = unit(0, "points"),
-      strip.text.y.left = element_text(face = "bold", angle = 0),
-      strip.background.y = element_blank(),
-      strip.placement = "outside",
-      axis.line = element_line()
-    )
-}
 
 # 2. collision~feature+covar -----------------
 
