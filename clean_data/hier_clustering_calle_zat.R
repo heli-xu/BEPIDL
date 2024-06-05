c1f58623725da0a5b123a7d640ee19675c4d82e7
Heli Xu <xuheli91@gmail.com>
2024-06-05 12:04:30 -0400

    add rd_type by zat to hier clustering, update plot

--- clean_data/hier_clustering_calle_zat.R
+++ clean_data/hier_clustering_calle_zat.R
@@ -12,20 +12,22 @@ sf_use_s2(FALSE)
 
 # import data --------------------
 calle_zat_xwalk <- readRDS("calle_zat_xwalk.rds")
-
-
 # NA is removed from xwalk
 # calle_zat_xwalk <- calle_zat_xwalk %>% 
 #   filter(!is.na(ZAT))
 
-calle2_zat_rep <- readRDS("../aggr_hclust_geo/calle_2_zat_rep.rds")
+#calle to zat w replicate 
+calle2_zat_rep <- readRDS("aggr_hclust_geo/calle_2_zat_rep.rds")
 
+
+zat_shapefile <- readRDS("ZAT/zat_shapefile.rds")
+
+#road type
+road_type <- readRDS("road_type/road_type_calle.rds") 
+
+# 1. Neighborhood distances -------------------------
 zat_shape_sub <- calle_2_zat_rep %>% 
   pull(ZAT)
-
-
-# neighborhood distances -------------------------
-zat_shapefile <- readRDS("ZAT/zat_shapefile.rds")
 
 centroid <- zat_shapefile %>% 
   st_zm() %>% 
@@ -34,7 +36,7 @@ centroid <- zat_shapefile %>%
 
 # to find out why centroid has 1 fewer row than zat_shape_sub
 # na <- bind_cols(shape = sort(zat_shape_sub[1:863]), centroid = sort(centroid$ZAT))
-# everything match... turns out there's an NA in zat_shape_sub!!
+# everything match... turns out there's an NA in zat_shape_sub!! --fixed from xwalk
 
 nb <- st_contiguity(zat_shapefile %>% 
                     filter(ZAT%in%zat_shape_sub) %>% 
@@ -48,26 +50,59 @@ library(sp)
 plot(as_Spatial(zat_shapefile %>% st_zm()), main = "Neighborhood Network")
 plot(nb, coords = coordinates(as_Spatial(centroid)), col="blue", add = TRUE)
 
-# 1. make a df of from-to-distance
+## 1.1 make a df of from-to-distance --------------
 n = length(dist_nb)
 res = data.table(from = integer(), to = integer(), dist = numeric())
 for(i in seq_len(n)){
   res = rbind(res, data.table(from = i, to = nb[[i]], dist = dist_nb[[i]]))
 }
 
-# 2. create network with cppRouting package
+## 1.2 create network with cppRouting package ---------------
 graph  <-  makegraph(res, directed = F)
 
-# 3. calculate distance with network topology (cppRouting package)
+## 1.3 calculate distance with network topology (cppRouting package) ----------
 dist_link <- get_distance_matrix(Graph=graph, 
                                  from = unique(res$from), 
                                  to = unique(res$to))
 
 #dist_lin2.rds saved##
 
-# clustering --------------------
-## cluster number ------------------
-D0 <- dist(calle_2_zat_rep)
+# 2. Join rd type-ZAT data-----------------
+## 2.1 road type in zat--------
+road_type_zat <- road_type %>% 
+  left_join(calle_zat_xwalk, by = "CodigoCL") %>% 
+  mutate(count = 1) %>% 
+  group_by(ZAT, MVITCla, road_type) %>% 
+  summarize(
+    sum = sum(count), .groups = "drop") %>% 
+  pivot_wider(id_cols = -MVITCla, 
+    names_from = "road_type", 
+    values_from = "sum")
+
+road_type %>% filter(is.na(road_type))
+
+road_type_zat_pct <- road_type_zat %>% 
+  mutate(across(everything(), ~replace_na(.x, 0))) %>% 
+  mutate(
+    total = rowSums(pick(Collector:Projected)),
+    across(Collector:Projected, ~(.x/total) * 100, .names = "pct_{.col}"),
+    pct_other = rowSums(pick(c(pct_Rural, pct_Pedestrian, pct_Unknown, pct_Projected)))
+  )
+
+saveRDS(road_type_zat_pct, file = "road_type/road_type_zat_pct.rds")
+
+## 2.2 add to zat data ----------------
+calle2zat_rep_rd <- calle2_zat_rep %>% 
+  left_join(road_type_zat_pct, by = "ZAT") %>% 
+  select(-c(Collector:total, pct_Rural, pct_Pedestrian, pct_Unknown, pct_Projected, pct_other)) %>% 
+  ## NOTE to leave out one pct_group to avoid collinearity
+  drop_na()
+
+saveRDS(calle2zat_rep_rd, file = "aggr_hclust_geo/rd_type/calle2zat_rep_rd.rds")
+
+# 3. clustering --------------------
+## 3.1 cluster number ------------------
+D0 <- dist(calle2zat_rep_rd)
 
 tree <- hclustgeo(D0)
 
@@ -79,7 +114,7 @@ rect.hclust(tree, k = 4, border = 1:4)
 legend("topright", legend = paste("cluster", 1:4),
        fill = 1:4, bty = "n", border = "white")
 
-## spatial constrainst --------------------
+## 3.2 spatial constrainst --------------------
 dist_link2 <- readRDS("aggr_hclust_geo/dist_link2.rds")
 
 D1 <- as.dist(dist_link2)
@@ -90,107 +125,80 @@ k <- 4
 cr <- choicealpha(D0, D1, range.alpha, k, graph = FALSE)
 
 plot(cr)
-# 0.25
+# 0.25 - no road type
+# 0.35 - yes road type - with or wo pct_other
 
-tree <- hclustgeo(D0, D1, alpha = 0.25)
+
+tree <- hclustgeo(D0, D1, alpha = 0.35)
 p4_nbdist <- cutree(tree, 4)
 
-# visualization ---------------
+# 4. visualization ---------------
 
 source("../functions/get_cluster.R")
 source("../functions/cluster_plot.R")
 pal <- c("#225ea8","#41b6c4","#a1dab4","#fecb3e") 
 
-calle2zat_clust <- get_cluster(calle_2_zat_rep, p4_nbdist)
+## df of zat-cluster, 
+## NOTE cluster vector is same order as ZAT column in D0 data, NOT the names of the vector
+zat_cluster <- data.frame(
+  ZAT = calle2zat_rep_rd$ZAT,
+  clus = p4_nbdist
+) 
 
+## 4.1 scaled data by cluster------
+calle2zat_clust <- get_cluster(calle2zat_rep_rd, p4_nbdist)
+
+clust_zat <- calle2zat_rep_rd %>% 
+  left_join(zat_cluster, by = "ZAT") 
+
+x2 <- clust_zat %>% filter(clus == 2) %>% 
+  mutate()
+
+## 4.2 cluster geo-----------
 calle2zat_geo <- zat_shapefile %>% 
   st_zm() %>% 
-  filter(ZAT%in%zat_shape_sub) %>% 
-  mutate(clus = p4_nbdist)
+  left_join(zat_cluster, by = "ZAT") %>% 
+  drop_na(clus)
 
+saveRDS(calle2zat_clust, file = "aggr_hclust_geo/rd_type/calle2zat_clust2.rds")
+saveRDS(calle2zat_geo, file = "aggr_hclust_geo/rd_type/calle2zat_geo2.rds")
+
+
+## 4.3 assemble plots------------
+## note the `fill` below, tho we ended up using a different route
 map <- ggplot()+
   geom_sf(data = zat_shapefile %>% st_zm())+
   geom_sf(data = calle2zat_geo, fill = pal[calle2zat_geo$clus])
-  
-
-(cluster_plot(calle2zat_clust) | map ) + 
-  plot_annotation('Hierarchical Clustering with Indicators and Neighborhood Constraint', 
-                  subtitle = 'ZAT level and aggregated calle level, Bogotá',
-                  theme=theme(plot.title=element_text(size=14, face = "bold", hjust=0.5),
-                              plot.subtitle = element_text(size = 10, face = "bold", hjust = 0.5)))+
-  plot_layout(widths = c(1,1.5), heights = unit(15, units = "cm"))
 
 # plot(calle2zat_geo$geometry, border = "grey", col  = p4_nbdist,
 #      main = "Partition p4_nb obtained with
 #          alpha=0.2 and neighborhood dissimilarities")
 
-saveRDS(calle2zat_clust, file = "aggr_hclust_geo/calle2zat_clust.rds")
-saveRDS(calle2zat_geo, file = "aggr_hclust_geo/calle2zat_geo.rds")
 
 #read in files above
 
 source("../functions/cluster_plot.R")
-# modify the ncol of facet
-cluster_plot <- function(cluster_data) {
-  cluster_data %>% 
-    ggplot() +
-    geom_col(aes(x = factor(indicator), y = scaled, fill = clus)) +
-    scale_fill_manual(values = pal)+
-    coord_flip() + 
-    geom_hline(yintercept = 0, linetype = "dotted")+ #still set as y (although it's after flipping)
-    theme_minimal() +
-    labs(y = "Relative Less   Relative More",
-      x = "") +
-    scale_x_discrete(expand = expansion(mult = 0.002),
-      labels = c(
-        "mean_BUSTOPDENS"="Bus stop density" ,
-        "mean_road_length_log" ="Road length",
-        "mean_st_4ln_length_log" ="Street length",
-        "mean_bikelane_m_log" ="Bikelane length",
-        "mean_sttree_per_km2" ="Tree",
-        "mean_bridg_per_km2" ="Bridge",
-        "mean_trlight_per_km2" ="Traffic light" ,
-        "mean_numrbp_per_km2" ="Bus route count" ,
-        "mean_numrt_per_km2" ="BRT route count",
-        "mean_bus_length_log" ="Bus route length",
-        "mean_brt_length_log"="Bus route length",
-        "mean_road_width"="Road width (st)",
-        # "mean_av_carrile"="Average lane",
-        "mean_area_roadway"="Roadway area (st)",
-        "mean_area_median"="Median area (st)",
-        "mean_area_sidewalk"="Sidewalk area (st)",
-        "mean_INTDENS"="Intersection density",
-        "mean_road_marks" ="Road marks (st)",
-        "mean_road_signs" = "Road signs (st)",
-        "mean_pedxwalk_signs"="Ped crosswalk sign (st)"
-      ))+
-    theme(panel.grid = element_blank(),
-      axis.text.x = element_blank(),
-      axis.text.y = element_text(size = 9),
-      axis.line.x = element_line(arrow = grid::arrow(length = unit(0.3, "cm"), 
-        ends = "both")),
-      plot.margin=grid::unit(c(0,0,0,0), "mm"),
-      strip.text = element_text(face = "bold", size = 10),
-      strip.background = element_rect(fill = "grey", color = "white"),
-      panel.spacing.x = unit(1, "lines")) +
-    facet_wrap(~clus, ncol = 2)
-}
 
 map <- ggplot()+
   geom_sf(data = zat_shapefile %>% st_zm())+
   geom_sf(data = calle2zat_geo, aes(fill = factor(clus)))+
   scale_fill_manual(values = pal)+
-  labs(fill = "Cluster")
+  labs(fill = "Cluster")+
+  theme_minimal()+
+  theme(
+    panel.grid = element_blank(),
+    axis.text = element_blank()
+  )
 
 (cluster_plot(calle2zat_clust) | map ) + 
   plot_annotation('Hierarchical Clustering with Indicators and Neighborhood Constraint', 
     subtitle = 'ZAT level and aggregated calle level, Bogotá',
     theme=theme(plot.title=element_text(size=14, face = "bold", hjust=0.5),
       plot.subtitle = element_text(size = 10, face = "bold", hjust = 0.5)))+
-  plot_layout(heights = unit(12, units = "cm"))
+  plot_layout(widths = c(1.5, 1), heights = unit(12, units = "cm"))
 
 
-# traffic flow (total trips) -----------
+# Extra: traffic flow (total trips) -----------
 zat_shapefile <- readRDS("ZAT/zat_shapefile.rds")
 total_trips <- readRDS("../data/zat_denom.rds")
 
@@ -217,7 +225,7 @@ ggplot()+
 
 saveRDS(walk_transit_geo, file = "aggr_hclust_geo/walk_transit_geo.rds")
 
-# population -----------------------------
+# Extra: population -----------------------------
 pop <- read_excel("../data/pop_zat.xlsx")
 
 zat_pop2021 <- pop %>% 
