488beb2440543582fc261ccacc3f09f7205b2097
Heli Xu <xuheli91@gmail.com>
2024-07-12 20:28:38 -0400

    cont and yn feature-collision in rd type stratified model

--- analysis/assoc_collision_street/rdtype_stratified_feature.R
+++ analysis/assoc_collision_street/rdtype_stratified_feature.R
@@ -43,7 +43,7 @@ other <- road_type %>%
 # 2. Join collision - feature, cov---------------------
 ## 2.1 features-----------
 ### tertile-------
-features <-  c(
+features_ter <-  c(
   "trees",
   "grade",
   "area_median",
@@ -68,18 +68,18 @@ features <-  c(
 )
 
 ### yn---------
-features <-  c(
+features_yn <-  c(
   "trees",
-  "grade",
+  #"grade", std error huge
   "area_median",
   "area_sidewalk",
-  "road_width",
+  #"road_width", all >0, exclude
   "road_marks",
   "warning_signs",
   "road_signs",
   "traffic_lights",
-  #"st_dir",  only 1,2 directions
-  "num_lanes_total",
+  # "st_dir", # not 1 and 0 , will be added for iterating
+  #"num_lanes_total",  all >0, exclude
   "pedxwalk_signs",
   "school_zone_signs",
   "stop_signs_v",
@@ -93,7 +93,7 @@ features <-  c(
 )
 
 ### cont-----------
-features <-  c(
+features_cont <-  c(
   "trees",
   "grade",
   "area_median",
@@ -138,7 +138,7 @@ covar <- covar_100 %>%
 ### merging-----------
 merge_calle_cont <- function(data){
   data2 <- data %>%
-    dplyr::select(codigocl, all_of(features)) %>%
+    dplyr::select(codigocl, all_of(features_cont)) %>%
     left_join(collision_calle
       %>% rename(codigocl = CodigoCL),
       by = "codigocl") %>%
@@ -152,12 +152,12 @@ merge_calle_cont <- function(data){
 
 merge_calle_tertile <- function(data){
   data2 <-data %>%
-    dplyr::select(codigocl, st_dir, all_of(features)) %>%
+    dplyr::select(codigocl, st_dir, all_of(features_ter)) %>%
     mutate(
-      across(all_of(features), ~ na_if(., 0)),
+      across(all_of(features_ter), ~ na_if(., 0)),
       #turn to NA so that it doesn't get computed
-      across(all_of(features), ~ ntile(., 3), .names = "{.col}"),
-      across(all_of(features), ~ replace_na(., 0)),
+      across(all_of(features_ter), ~ ntile(., 3), .names = "{.col}"),
+      across(all_of(features_ter), ~ replace_na(., 0)),
       #turn it back 0
       across(-codigocl, ~ factor(., levels = c("1", "0", "2", "3")))
     ) %>%
@@ -173,8 +173,8 @@ merge_calle_tertile <- function(data){
 
 merge_calle_yn <- function(data){
   data2 <- data %>%
-    dplyr::select(codigocl, st_dir, all_of(features)) %>%
-    mutate(across(all_of(features), ~ if_else(. > 0, 1, 0)),
+    dplyr::select(codigocl, st_dir, all_of(features_yn)) %>%
+    mutate(across(all_of(features_yn), ~ if_else(. > 0, 1, 0)),
       across(-codigocl, ~ factor(.))) %>%
     left_join(collision_calle
       %>% rename(codigocl = CodigoCL),
@@ -186,38 +186,43 @@ merge_calle_yn <- function(data){
   return(data3)
 }
 
-## 2.3 join covar (for cont)---------------
+## choose one of below
+## 2.3 join covar-cont ---------------
+#assign features_cont
 arterial_collision <- merge_calle_cont(arterial)
-# arterial_collision <- merge_calle_yn(arterial)
-# arterial_collision <- merge_calle_tertile(arterial)
-
 collector_collision <- merge_calle_cont(collector)
-# collector_collision <- merge_calle_yn(collector)
-
-
 local_collision <- merge_calle_cont(local)
-# local_collision <- merge_calle_yn(local)
-
 other_collision <- merge_calle_cont(other)
 
-# 3. Model: collision-feature------------------
-fit_feature <- glm.nb(death ~trees + pop_density + pct_male + pct_yr_0_9 + pct_yr_10_19 + pct_yr_30_39 + pct_yr_40_49 + pct_yr_50_59 + pct_yr_60_69 + pct_yr_70_79 + pct_yr_80_plus +ses_cat_r, data = arterial_collision)
+## 2.4 join covar-yn --------
+#assign features_yn
+arterial_collision <- merge_calle_yn(arterial)
+collector_collision <- merge_calle_yn(collector)
+local_collision <- merge_calle_yn(local)
+other_collision <- merge_calle_yn(other)
 
-fit_feature <- glm.nb(total ~trees + pct_apt + pct_home + pct_unoccu + pop_density + pct_male + pct_yr_0_9 + pct_yr_10_19 + pct_yr_30_39 + pct_yr_40_49 + pct_yr_50_59 + pct_yr_60_69 + pct_yr_70_79 + pct_yr_80_plus + +ses_cat_r, data = collector_collision)
+## 2.5 join covar-ter-----------
+arterial_collision <- merge_calle_tertile(arterial)
+collector_collision <- merge_calle_tertile(collector)
+local_collision <- merge_calle_tertile(local)
+other_collision <- merge_calle_tertile(other)
 
-fit_feature <- glm.nb(total ~trees + pct_apt + pct_home + pct_unoccu + pop_density + pct_male + pct_yr_0_9 + pct_yr_10_19 + pct_yr_30_39 + pct_yr_40_49 + pct_yr_50_59 + pct_yr_60_69 + pct_yr_70_79 + pct_yr_80_plus + +ses_cat_r, data = local_collision)
+# 3. Model: collision-feature ------------------
+## test(no need to run)
+fit_feature <- glm.nb(total ~trees + pct_apt + pct_home + pct_unoccu + pop_density + pct_male + pct_yr_0_9 + pct_yr_10_19 + pct_yr_30_39 + pct_yr_40_49 + pct_yr_50_59 + pct_yr_60_69 + pct_yr_70_79 + pct_yr_80_plus +ses_cat_r, data = local_collision)
 
 summary(fit_feature)
+##end
 
 fit_feature_strat <- function(predictor, data){
-  formula <- as.formula(paste("total ~", predictor, "+ pct_apt + pct_home + pct_unoccu + pop_density + pct_male + pct_yr_0_9 + pct_yr_10_19 + pct_yr_30_39 + pct_yr_40_49 + pct_yr_50_59 + pct_yr_60_69 + pct_yr_70_79 + pct_yr_80_plus"))
+  formula <- as.formula(paste("total ~", predictor, "+ pct_apt + pct_home + pct_unoccu + pop_density + pct_male + pct_yr_0_9 + pct_yr_10_19 + pct_yr_30_39 + pct_yr_40_49 + pct_yr_50_59 + pct_yr_60_69 + pct_yr_70_79 + pct_yr_80_plus + ses_cat_r"))
   
   model <- glm.nb(formula,  data = data)
   return(model)
 }
 
-test <- fit_feature_strat("pedxwalk_signs", arterial_collision)
-#tertile features[[6,7, 8,10**, 13,14, 16]] as cont var
+test <- fit_feature_strat("grade", arterial_collision)
+#features[[6,7, 8,10**, 13,14, 16]] as cont var
 #Warning: fitted rates numerically 0 occurred
 #**Error: no valid set of coefficients has been found: please supply starting values
 
@@ -225,6 +230,8 @@ summary(test)
 df <- tidy(test)
 ## 3.1 Continous----------
 ### 3.1.1 arterial-----------
+features <- features_cont
+
 fit_allfea_art <- map(features, \(x) fit_feature_strat(x, data = arterial_collision))
 
 feature_df <- map_df(fit_allfea_art,
@@ -284,3 +291,123 @@ fit_allfea_loc <- map(features, \(x) fit_feature_strat(x, data = local_collision
 ### 3.1.4 other---------
 fit_allfea_oth <- map(features, \(x) fit_feature_strat(x, data = other_collision))
 #didn't converge and many more warnings
+
+## 3.2 Binary-----------
+### 3.2.1 arterial-------------
+features <- c("st_dir", features_yn)
+
+fit_allfea_art <- map(features, \(x) fit_feature_strat(x, data = arterial_collision))
+
+feature_df <- map_df(fit_allfea_art,
+  \(x) tidy(x, conf.int = TRUE, exponentiate = TRUE))
+
+art_yn_RR <- feature_df %>%
+  mutate(
+    road_type = "arterial",
+    across(where(is.numeric), ~round(., 4)),
+    RR_95CI = paste0(estimate," (", conf.low, ",", conf.high, ")")
+  ) %>% 
+  dplyr::select(
+    road_type,
+    term,
+    RR_95CI,
+    estimate,
+    std.error,
+    conf.low,
+    conf.high,
+    p.value,
+    statistic
+  )
+
+save(art_yn_RR, file = "gis_rdtype_stratified/art_yn_RR.rds")
+
+### 3.2.2 collector-----------
+fit_allfea_col <- map(features, \(x) fit_feature_strat(x, data = collector_collision))
+
+feature_col_df <- map_df(fit_allfea_col,
+  \(x) tidy(x, conf.int = TRUE, exponentiate = TRUE))
+
+col_yn_RR <- feature_col_df %>% 
+  mutate(
+    road_type = "collector",
+    across(where(is.numeric), ~round(., 4)),
+    RR_95CI = paste0(estimate," (", conf.low, ",", conf.high, ")")
+  ) %>% 
+  dplyr::select(
+    road_type,
+    term,
+    RR_95CI,
+    estimate,
+    std.error,
+    conf.low,
+    conf.high,
+    p.value,
+    statistic
+  )
+
+saveRDS(col_yn_RR, file = "gis_rdtype_stratified/col_yn_RR.rds")
+
+### 3.2.3 local----------
+fit_allfea_loc <- map(features, \(x) fit_feature_strat(x, data = local_collision))
+
+feature_loc_df <- map_df(fit_allfea_loc,
+  \(x) tidy(x, conf.int = TRUE, exponentiate = TRUE))
+
+loc_yn_RR <- feature_col_df %>% 
+  mutate(
+    road_type = "collector",
+    across(where(is.numeric), ~round(., 4)),
+    RR_95CI = paste0(estimate," (", conf.low, ",", conf.high, ")")
+  ) %>% 
+  dplyr::select(
+    road_type,
+    term,
+    RR_95CI,
+    estimate,
+    std.error,
+    conf.low,
+    conf.high,
+    p.value,
+    statistic
+  )
+
+saveRDS(loc_yn_RR, file = "gis_rdtype_stratified/loc_yn_RR.rds")
+
+### 3.2.4 other-------
+fit_allfea_oth <- map(features, \(x) fit_feature_strat(x, data = other_collision))
+#didn't converge and other warnings
+
+## 3.3 Tertile -----------
+### 3.3.1 arterial--------
+features <- c("st_dir", features_ter)
+
+fit_allfea_art <- map(features, \(x) fit_feature_strat(x, data = arterial_collision))
+
+feature_df <- map_df(fit_allfea_art,
+  \(x) tidy(x, conf.int = TRUE, exponentiate = TRUE))
+
+art_ter_RR <- feature_df %>%
+  mutate(
+    road_type = "arterial",
+    across(where(is.numeric), ~round(., 4)),
+    RR_95CI = paste0(estimate," (", conf.low, ",", conf.high, ")")
+  ) %>% 
+  dplyr::select(
+    road_type,
+    term,
+    RR_95CI,
+    estimate,
+    std.error,
+    conf.low,
+    conf.high,
+    p.value,
+    statistic
+  )
+
+saveRDS(art_ter_RR, file = "gis_rdtype_stratified/art_ter_RR.rds")
+
+### 3.3.2 collector--------
+fit_allfea_col <- map(features, \(x) fit_feature_strat(x, data = collector_collision))
+
+feature_col_df <- map_df(fit_allfea_art,
+  \(x) tidy(x, conf.int = TRUE, exponentiate = TRUE))
