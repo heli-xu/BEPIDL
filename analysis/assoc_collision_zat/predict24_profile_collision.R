f98337f9bea6478f7526626561d4b90a4453caca
Heli Xu <xuheli91@gmail.com>
2024-08-19 17:04:37 -0400

    zat level collision~profile w 2015-19 predict data w covar ses or ipm
    reference level profile3, updated population density (adj by km2)

--- analysis/assoc_collision_zat/predict24_profile_collision.R
+++ analysis/assoc_collision_zat/predict24_profile_collision.R
@@ -8,14 +8,17 @@ col_ped_zat <- readRDS("../../clean_data/collision/collision_zat_df.rds")
 # profile <- readRDS("../../clean_data/aggr_hclust_geo/calle2zat_geo.rds") %>% 
 #   st_drop_geometry()
 
-# predict24_profile with road network characteristics
-profile <- readRDS("../../clean_data/predict24/w_road_info/pr_zat_cluster_rd.rds")
+# predict24_profile with road network characteristics - 2015-2019 only
+profile <- readRDS("../../clean_data/predict24/w_road_info/2015_19/pr_zat_cluster_rd.rds")
 
 # if not in profile, road type area as covar
 road_type <- readRDS("../../clean_data/road_type/rd_type_area_zat.rds")
 
 #ses
-ses_zat <- readRDS("../../clean_data/ses/ses_zat.rds")
+ses_zat <- readRDS("../../clean_data/covar_mzn/ses_zat.rds")
+
+#ipm
+ipm <- readRDS("../../clean_data/covar_mzn/ipm_zat.rds")
 
 #covar, offset
 traffic <- readRDS("../../clean_data/ZAT/walk_pubt.rds")
@@ -27,13 +30,15 @@ pop_density <- readRDS("../../clean_data/ZAT/pop_density2021.rds")
 source("../../functions/distr_stat.R")
 
 profile_distr <- distr_stat(profile, ZAT, clus) %>% 
-  mutate(clus = factor(clus, levels = c(5, 1, 2, 3, 4)))
+  mutate(clus = factor(clus, levels = c(3, 2, 1, 4)))
+
+write_csv(profile_distr, file = "predict24_profile/2015_19/predict24_1519_descrp.csv")
 
 # 2. Collision ~ profile ----------------------------------
 ## 2.1 join data -------------
 profile_ped_zat <- col_ped_zat %>% 
   left_join(profile, by = "ZAT") %>% 
-  mutate(clus = factor(clus, levels = c(5, 1, 2, 3, 4))) %>% 
+  mutate(clus = factor(clus, levels = c(3, 2, 1, 4))) %>% 
   drop_na(clus)
 
 ## 2.2 injury --------------
@@ -183,7 +188,7 @@ profile_ses_covar <- ses_zat %>%
   dplyr::select(ZAT, ses_cat) %>% 
   mutate(ses_cat_r = factor(ses_cat, levels = rev(levels(ses_cat)))) %>% 
   left_join(profile, by = "ZAT") %>% 
-  mutate(clus = factor(clus, levels = c(4, 1, 2, 3, 5))) %>% 
+  mutate(clus = factor(clus, levels = c(3,1, 2, 4))) %>% 
   #drop_na(clus) %>% 
   left_join(col_ped_zat, by = "ZAT") %>% 
   left_join(traffic %>% 
@@ -195,6 +200,7 @@ profile_ses_covar <- ses_zat %>%
   ) %>% 
   drop_na() %>% 
   filter(walk_pubt > 0)
+
 
 ## 4.2 Model ------------
 ### injury ----------
@@ -270,11 +276,12 @@ rd_type_zat <- road_type %>%
 #leave out the *reference group* to avoid colinearity --tried local
 #arterial
 
+### using SES-----
 profile_covar_rd <- ses_zat %>% 
   dplyr::select(ZAT, ses_cat) %>% 
   mutate(ses_cat_r = factor(ses_cat, levels = rev(levels(ses_cat)))) %>% 
   left_join(profile, by = "ZAT") %>% 
-  mutate(clus = factor(clus, levels = c(5, 1, 2, 3, 4))) %>% 
+  mutate(clus = factor(clus, levels = c(3,1, 2, 4))) %>% 
   #drop_na(clus) %>% 
   left_join(col_ped_zat, by = "ZAT") %>% 
   left_join(traffic %>% 
@@ -288,40 +295,73 @@ profile_covar_rd <- ses_zat %>%
   ) %>% 
   filter(walk_pubt > 0)
 
+### using IPM---------
+profile_ipm_covar <- profile %>% 
+  left_join(ipm, by = "ZAT") %>% 
+  mutate(clus = factor(clus, levels = c(3,1, 2, 4))) %>% 
+  #drop_na(clus) %>% 
+  left_join(col_ped_zat, by = "ZAT") %>% 
+  left_join(traffic %>% 
+      dplyr::select(ZAT, walk_pubt), 
+    by = "ZAT") %>% 
+  left_join(pop_density, by = "ZAT") %>% 
+  left_join(rd_type_zat, by = "ZAT") %>%  
+  drop_na() %>% 
+  mutate(
+    across(c(pop_density, starts_with("pcta_")), ~scale(.x)[, 1])
+  ) %>% 
+  filter(walk_pubt > 0)
+
+
 ## 5.2 Model ------------
 ### injury ----------
+### SES
 injury_co3 <- glm.nb(injury ~ clus + ses_cat_r + offset(log(walk_pubt)) + pop_density + pcta_Collector + pcta_Local + pcta_other, data = profile_covar_rd)
+### IPM
+injury_co3 <- glm.nb(injury ~ clus + MEANIPM + offset(log(walk_pubt)) + pop_density + pcta_Collector + pcta_Local + pcta_other, data = profile_ipm_covar)
+
 summary(injury_co3)
 
 car::vif(injury_co3)
 #pcta_Arterial moderate multicollinearity by adjusted GVIF (1-2.5 acceptable)
 #so set arterial as reference instead
+# GVIF^(1/(2*Df)) < 2: Indicates low multicollinearity, which is generally acceptable.
 
 injury_co3_df <-  tidy(injury_co3, conf.int = TRUE, exponentiate = TRUE) %>%
   mutate(outcome = "injury")
 
 ### death -------
+##SES
 death_co3 <- glm.nb(death ~ clus + ses_cat_r + offset(log(walk_pubt)) + pop_density + pcta_Collector + pcta_Local + pcta_other, data = profile_covar_rd)
 summary(death_co3)
+
+##IPM
+death_co3 <- glm.nb(death ~ clus + MEANIPM + offset(log(walk_pubt)) + pop_density + pcta_Collector + pcta_Local + pcta_other, data = profile_ipm_covar)
+
 
 death_co3_df <- tidy(death_co3, conf.int = TRUE, exponentiate = TRUE) %>%
   mutate(outcome = "death")
 
 ### total -------
-total_co3 <- glm.nb(total ~ clus + ses_cat_r + offset(log(walk_pubt)) + pop_density + pcta_Collector + pcta_Local + pcta_other, data = profile_covar_rd)
+##SES
+total_co3 <- glm.nb(total ~ clus + offset(log(walk_pubt)) + ses_cat_r + pop_density + pcta_Collector + pcta_Local + pcta_other, data = profile_covar_rd)
+
+##IPM
+total_co3 <- glm.nb(total ~ clus + MEANIPM + offset(log(walk_pubt)) + pop_density + pcta_Collector + pcta_Local + pcta_other, data = profile_ipm_covar)
 
 total_co3_df <- tidy(total_co3, conf.int = TRUE, exponentiate = TRUE) %>% 
   mutate(outcome = "total")
 
 ## 5.3 summarise RR------------
-prof_ses_covar_rd_RR <- bind_rows(injury_co3_df, death_co3_df, total_co3_df) %>% 
-  mutate(RR_95CI = paste0(round(estimate,2)," (", round(conf.low,2), ",", round(conf.high, 2), ")")) %>%
+prof_covar_rd_RR <- bind_rows(injury_co3_df, death_co3_df, total_co3_df) %>% 
+  mutate(
+    across(where(is.numeric), ~round(., 4)),
+    RR_95CI = paste0(round(estimate,2)," (", round(conf.low,2), ",", round(conf.high, 2), ")")) %>%
   mutate(predictor = case_match(
     term,
-    "(Intercept)" ~ "profile_5",
+    "(Intercept)" ~ "profile_3",
     "clus1" ~ "profile_1",
     "clus2" ~ "profile_2",
-    "clus3" ~ "profile_3",
     "clus4" ~ "profile_4",
     # "ses_cat_r5" ~ "ses_5",
     # "ses_cat_r4" ~ "ses_4",
@@ -329,20 +369,29 @@ prof_ses_covar_rd_RR <- bind_rows(injury_co3_df, death_co3_df, total_co3_df) %>%
     # "ses_cat_r2" ~ "ses_2",
     # "ses_cat_r1" ~ "ses_1",
     .default = "(Covariates)"
-  )) 
+  ),
+    .before = "term") 
 
-saveRDS(prof_ses_covar_rd_RR, file = "predict24_profile/profile_rd_covar2_RR_ref5.rds")
+saveRDS(prof_covar_rd_RR, file = "predict24_profile/2015_19/profile_ses_RR_ref3.rds")
 
-prof_ses_covar_RR_csv <- prof_ses_covar_RR %>% 
+saveRDS(prof_covar_rd_RR, file = "predict24_profile/2015_19/profile_ipm_RR_ref3.rds")
+
+prof_covar_RR_csv <- prof_ses_covar_rd_RR %>% 
   dplyr::select(
-    term, predictor,
+    predictor,
+    term,
+    outcome,
     RR_95CI,
-    p.value,
-    outcome
-  )
+    everything()
+  ) %>% 
+  rename(z_value = statistic)
+
+write_csv(prof_covar_RR_csv, file = "predict24_profile/2015_19/zat_profile_predict1519_ses_ref3_RR.csv")
+
+write_csv(prof_covar_RR_csv, file = "predict24_profile/2015_19/zat_profile_predict1519_ipm_ref3_RR.csv")
 
 ## 5.4 visualize --------
-prof_ses_covar_rd_RR <- readRDS("predict24_profile/profile_rd_covar2_RR.rds")
+#prof_ses_covar_rd_RR <- readRDS("predict24_profile/profile_rd_covar2_RR.rds")
 source("../../functions/plot_RR.R")
 
 prof_ses_covar_rd_RR %>% 
@@ -351,11 +400,12 @@ prof_ses_covar_rd_RR %>%
   facet_grid(vars(outcome), switch = "y")+
   labs(
     title = "Pedestrian Collision and Neighborhood (ZAT) Profiles in Bogotá",
-    subtitle = "Adjusted for ZAT-level SES, walking/public transit trips and population density",
+    subtitle = "Adjusted for ZAT-level walking/public transit trips, SES, \nroad types and population density",
     x = "RR (95%CI)",
     y = "ZAT Profile",
-    caption = "All comparisons are relative to the profile 1."
+    caption = "All comparisons are relative to the profile 3."
   )+
   theme(
     plot.title.position = "plot"
   )
+
